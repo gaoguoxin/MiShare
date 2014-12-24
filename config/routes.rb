f732ed771defe6ef07811271d5a33970fc640030
Davis Gao <naitnix@126.com>
2014-12-24 14:32:37 +0800

    m

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,14 @@
 Rails.application.routes.draw do
 
+  resources :animals do 
+    collection do 
+      get 'result'
+      get 'answer'
+      get 'check'
+      post 'set_answer'
+    end    
+  end
+
   resources :lyrics do 
     collection do 
       get 'result'
